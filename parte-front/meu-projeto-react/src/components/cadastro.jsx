fb1d66a1b55026f3bdd555faa7a1b9c1792cb7d1
PEDRO MACHADO DA SILVA <02339335060@senacrs.edu.br>
2025-09-18 14:00:28 -0300

    blabla

--- parte-front/meu-projeto-react/src/components/cadastro.jsx
+++ parte-front/meu-projeto-react/src/components/cadastro.jsx
@@ -40,26 +40,24 @@ export default function Cadastro() {
     }
   }
 
-
   return (
     <div className="container">
       <section className="auth-section">
         <h2>Mount & Fundle</h2>
-        <form  onSubmit={handleSubmit} className="auth-form">
+        <form className="auth-form">
           <label htmlFor="nome">Nome:</label>
-          <input type="text" id="nome" name="nome" placeholder="Digite seu nome"  value={name}  onChange={(e) => setName(e.target.value)} required />
+          <input type="text" id="nome" name="nome" placeholder="Digite seu nome" required/>
           <label htmlFor="email">E-mail:</label>
 
-          <input type="email" id="email" name="email" placeholder="Digite seu e-mail" value={email}   onChange={(e) => setEmail(e.target.value)}required />
+          <input type="email" id="email" name="email" placeholder="Digite seu e-mail" required/>
           <label htmlFor="senha">Senha:</label>
 
-          <input type="password" id="senha" name="senha" placeholder="Digite sua senha"  value={password}onChange={(e) => setPassword(e.target.value)}   required
-/>
+          <input type="password" id="senha" name="senha" placeholder="Digite sua senha" required />
 
-          <button type="submit" className="btn" id="botao-registro"> Cadastrar</button>
-
+          <button type="submit" className="btn"> Cadastrar</button>
+          
           <p className="login-link" onClick={() => {
-            navigate("/login")
+           navigate("/login")
           }}>Já possui um personagem? Faça Login</p>
         </form>
         <p id="mensagem"></p>
