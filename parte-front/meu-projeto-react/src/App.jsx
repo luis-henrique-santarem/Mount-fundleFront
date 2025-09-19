f57b7adb6d16363af303b700ed3587fc1466d588
LUIS HENRIQUE PEDROSO SANTAREM <04674231051@senacrs.edu.br>
2025-09-19 16:27:15 -0300

    feat: adicionado o RingScreen

--- parte-front/meu-projeto-react/src/App.jsx
+++ parte-front/meu-projeto-react/src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Cadastro from "./components/cadastro";
 import Login from "./components/loginUser";
+import RingScreen from "./components/ringscreen";
 
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/cadastro" />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Navigate to="/ringscreen" />} />
+<Route path="/ringscreen" element={<RingScreen />} />
+
+       {/*<Route path="/" element={<Navigate to="/cadastro" />} /> */}  
+        {/*<Route path="/cadastro" element={<Cadastro />} /> */} 
+        {/* <Route path="/login" element={<Login />} /> */} 
       </Routes>
     </Router>
   );
--- parte-front/meu-projeto-react/src/components/loginUser.jsx
+++ parte-front/meu-projeto-react/src/components/loginUser.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import './login.css'
-import { useState } from "react";
 
 function login() {
 
